2d4f4e5cb99305f40247ed24146ecae96833b17e
Denis Doich <doich225@gmail.com>
2022-08-20 19:02:39 +0300

    80% working

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,71 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class AdminController {
+
+    private final UserService userService;
+
+    public AdminController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/admin")
+    public String findAll (Model model){
+        List <User> userList = userService.findAll();
+        model.addAttribute("userlist", userList);
+        return "users";
+
+    }
+//    @GetMapping("/users/create")
+//    public String createUserForm (User user){
+//        return "create";
+//    }
+//
+//    @PostMapping("/users/create")
+//    public String createUser (User user){
+//        userService.save(user);
+//        return "redirect:/users";
+//    }
+
+    @GetMapping("/user/{id}")
+    public String read(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userService.findById(id));
+        return "/read";
+    }
+
+    @GetMapping("/user")
+    public String home(Principal principal, Model model) {
+        model.addAttribute("user",userService.findByUsername(principal.getName()));
+        return "/read";
+    }
+
+    @GetMapping("/admin/{id}/edit")
+    public String edit (Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.findById(id));
+        return "/edit";
+    }
+
+    @PostMapping("/admin/{id}")
+    public String update (@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
+    @GetMapping("/admin/{id}/delete")
+    public String delete(@PathVariable("id") Long id){
+        userService.delete(id);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,85 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public void setbCryptPasswordEncoder(@Lazy BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public boolean save (User user) {
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDB != null) {
+            return false;
+        }
+
+        user.setRoles(Collections.singleton(new Role(1L, "ROLE_USER")));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
+    }
+
+    public User findById (Long id){
+        return userRepository.findById(id).orElse(null);
+    }
+    public List<User> findAll (){
+        return userRepository.findAll();
+    }
+    public void delete (Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = findByUsername(username);
+        if(user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                mapRolesToAuthorities(user.getRoles()));
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getName())).collect(Collectors.toList());
+    }
+}
